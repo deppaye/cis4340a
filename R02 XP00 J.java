b09e9549b7b695b5fb06e0a0adadf431ba130c48
deppaye <alexdeppe6@gmail.com>
2026-03-08 17:30:49 +0000

    Add lab files

---
+++ R02 XP00 J.java
@@ -0,0 +1,17 @@
+//Non-compliant
+public void deleteFile() {
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
+}
+
+//Compliant
+public void deleteFile() {
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+        System.out.println("Failed to delete file.");
+    }
+}
